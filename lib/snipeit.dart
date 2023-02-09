5da5f03a6d499db901bca1a7e40b69119adedbc6
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-02-09 14:01:14 -0500

    rename token to accessToken

--- lib/snipeit.dart
+++ lib/snipeit.dart
@@ -2,14 +2,13 @@ import 'package:http/http.dart' as http;
 
 class SnipeIT {
   String url;
-  String token;
+  String accessToken;
   http.Client _client;
 
   /// Represents a SnipeIT installation
   ///
   /// [url] the baseurl to perform REST API calls against
-  /// [token] the API token
-  SnipeIT({required this.url, required this.token, http.Client? client})
+  /// [accessToken] the API Key for authenticating with the API
       : _client = client ?? http.Client();
 
 
