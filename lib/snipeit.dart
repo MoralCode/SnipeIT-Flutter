8a50512b6ebdcf3974767f6a70559768d86e5346
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-02-09 14:00:28 -0500

    allow a custom http client to be provided for later testing

--- lib/snipeit.dart
+++ lib/snipeit.dart
@@ -1,6 +1,16 @@
+import 'package:http/http.dart' as http;
+
 class SnipeIT {
   String url;
   String token;
+  http.Client _client;
 
-  SnipeIT({required this.url, required this.token});
+  /// Represents a SnipeIT installation
+  ///
+  /// [url] the baseurl to perform REST API calls against
+  /// [token] the API token
+  SnipeIT({required this.url, required this.token, http.Client? client})
+      : _client = client ?? http.Client();
+
+
 }
