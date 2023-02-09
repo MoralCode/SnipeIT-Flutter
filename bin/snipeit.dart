91e2d8e872cf574a5939baeb4a337d7bcf4ec4d3
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-02-09 11:54:51 -0500

    dart create

---
+++ bin/snipeit.dart
@@ -0,0 +1,5 @@
+import 'package:snipeit/snipeit.dart' as snipeit;
+
+void main(List<String> arguments) {
+  print('Hello world: ${snipeit.calculate()}!');
+}
---
+++ test/snipeit_test.dart
@@ -0,0 +1,8 @@
+import 'package:snipeit/snipeit.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
