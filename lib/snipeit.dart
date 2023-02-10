341bc2e03879e8f7da9b744e216a86841d188fae
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-02-09 19:48:16 -0500

    set up url building process

--- lib/snipeit.dart
+++ lib/snipeit.dart
@@ -1,17 +1,21 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class SnipeIT {
-  String url;
+  Uri _url;
   String accessToken;
   http.Client _client;
+  final String apiPrefix = "api/v1";
 
   /// Represents a SnipeIT installation
   ///
   /// [url] the baseurl to perform REST API calls against
   /// [accessToken] the API Key for authenticating with the API
   /// [client] an (optional) HTTP client to use for accessing the API. Useful for unit testing.
-  SnipeIT({required this.url, required this.accessToken, http.Client? client})
-      : _client = client ?? http.Client();
+  SnipeIT({required String url, required this.accessToken, http.Client? client})
+      : _url = Uri.parse(url),
+        _client = client ?? http.Client();
 
   Map<String, String> _getHeaders() {
     return {
